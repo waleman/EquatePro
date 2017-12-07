98abeba6e8fb1d031c45af081c6c65d508a62fb9
Jose Aleman <w.aleman.hn@gmail.com>
2017-12-07 17:10:12 -0600

    Cambios en Facturar profuctos

--- EquatePro/src/pages/profuctos-facturar/profuctos-facturar.ts
+++ EquatePro/src/pages/profuctos-facturar/profuctos-facturar.ts
@@ -12,9 +12,31 @@ export class ProfuctosFacturarPage {
 
   public datos :any;
 
+  public _id:any;
+  public _codigo:any;
+  public _producto:any;
+  public _cantidad:any;
+  public _precioCosto:any;
+  public _impuesto:any;
+
+  public cantidad:number = 1;
+  public impuesto:any= '0.00';
+  public subtotal:any= '0.00';
+  public total:any= '0.00';
+  public descuento:any= '0.00';
+
+
+
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.datos = this.navParams.get('datos')
-    console.log(this.datos)
+    this._id = this.datos['productoId'];
+    this._codigo = this.datos['codigo'];
+    this._producto = this.datos['producto'];
+    this._cantidad = this.datos['cantidad'];
+    this._precioCosto = this.datos['precioCosto'];
+    this._impuesto = this.datos['porcentajeImpuesto'];
+
   }
 
   ionViewDidLoad() {
@@ -22,6 +44,6 @@ export class ProfuctosFacturarPage {
   }
 
 
-  
+
 
 }
