29903933cf1946e63a37155e90b2597c820415e9
Jose Aleman <w.aleman.hn@gmail.com>
2017-12-16 12:30:26 -0600

    Cambion en la parte de creacion de ventas

--- EquatePro/src/pages/profuctos-facturar/profuctos-facturar.ts
+++ EquatePro/src/pages/profuctos-facturar/profuctos-facturar.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+//controladores
+import { Storage } from '@ionic/storage';
+import { NgProbeToken } from '@angular/core/src/application_ref';
+import { ViewController } from 'ionic-angular';
+
 
 
 @IonicPage()
@@ -22,13 +27,21 @@ export class ProfuctosFacturarPage {
   public cantidad:number = 1;
   public impuesto:any= '0.00';
   public subtotal:any= '0.00';
+  public neto:any ='0.00';
   public total:any= '0.00';
   public descuento:any= '0.00';
+  public medida:any;
+
+  public ListaPrecios=[];
+  public tipoprecio :any ;
+  public unidades:any;
+  public precio:any='0.00';
 
 
 
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public storageCrtl: Storage, public viewCtrl: ViewController) {
     this.datos = this.navParams.get('datos')
     this._id = this.datos['productoId'];
     this._codigo = this.datos['codigo'];
@@ -37,11 +50,73 @@ export class ProfuctosFacturarPage {
     this._precioCosto = this.datos['precioCosto'];
     this._impuesto = this.datos['porcentajeImpuesto'];
 
+
+    this.RecolectarPrecios(this._codigo, this._codigo);
   }
 
   ionViewDidLoad() {
     
   }
+
+RecolectarPrecios(productoCodigo,tipoprecio){
+
+  this.storageCrtl.ready().then(() => {
+    this.storageCrtl.get("precios").then(data => {
+            for(let valores of data){
+                if(valores.productoId == this._id){
+                  this.ListaPrecios.push(valores);
+                }
+             
+            }
+       });
+      });
+}
+
+
+SeleccionarPrecio(){
+
+
+  for(let val of this.ListaPrecios){
+    if (val["precioId"] == this.tipoprecio ){
+        this.precio = val.precio;
+      this.precio = this.formatNum(this.precio);
+        this.unidades = val.unidades
+      this.Calcular()
+    }
+  }
+ 
+} 
+
+
+
+Calcular(){
+  this.subtotal = this.precio * this.cantidad;
+  this.subtotal = this.formatNum(this.subtotal);
+  this.neto = this.subtotal - this.descuento;
+  this.neto = this.formatNum(this.neto);
+  this.impuesto = this.neto * this._impuesto;
+  this.impuesto = this.formatNum(this.impuesto);
+  this.total =  this.neto - this.impuesto;
+  this.total = this.formatNum(this.total);
+
+
+}
+
+
+ formatNum(val) {
+  val = Math.round(val * 100) / 100;
+  val = ("" + val).indexOf(".") > -1 ? val + "00" : val + ".00";
+  var dec = val.indexOf(".");
+  return dec == val.length - 3 || dec == 0 ? val : val.substring(0, dec + 3);
+}
+
+
+  ocultar(){
+    this.viewCtrl.dismiss();
+  }
+
+
+
 
 
 
