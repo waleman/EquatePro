22f3cd70149d10cfcd1044e958d667f8a8508bbd
Jose Aleman <w.aleman.hn@gmail.com>
2017-12-28 11:40:22 -0600

    coregido errores con la pantalla de sincronizar

--- EquatePro/src/pages/sincronizar/sincronizar.ts
+++ EquatePro/src/pages/sincronizar/sincronizar.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController, AlertController } from 'ionic-angular';
 
 //pagina 
 import { HomePage } from '../home/home';
@@ -10,6 +10,7 @@ import { UtilidadesProvider } from '../../providers/utilidades/utilidades';
 import { ToastController } from 'ionic-angular';
 import { Http, Headers } from '@angular/http';
 import { Storage } from '@ionic/storage';
+import { Network } from '@ionic-native/network';
 
 @IonicPage()
 @Component({
@@ -17,24 +18,76 @@ import { Storage } from '@ionic/storage';
   templateUrl: 'sincronizar.html',
 })
 export class SincronizarPage {
-
+  public keys:any;
   public vendedorId:any;
   public fechaA : any;
+  public cantidadpedidos:any = '0';
+  public cantidadClientes:any = '0';
+  public cantidadClientesVisitados:any = '0';
+  public TipoConexion:any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public _conexion: ConexionProvider, public _utilidades: UtilidadesProvider, public StrCrl: Storage, public http: Http) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public _conexion: ConexionProvider, public _utilidades: UtilidadesProvider, public StrCrl: Storage, public http: Http,
+    public alertCtrl: AlertController, public loadingCtrl: LoadingController, private networkCtrl: Network) {
+    this.datosParaMostrar();
     this.DatosdeUsuario();
     this.fechaA =  this.fecha()
   }
   
 
-  ionViewDidLoad() {
-    
+ /*recolectar estadisticas */
+  datosParaMostrar(){
+        this.StrCrl.ready().then(()=>{
+            this.StrCrl.keys().then(llaves =>{
+              this.keys = llaves;
+                  if(this.keys.includes('pedidos')){
+                        this.StrCrl.get('pedidos').then(datos => {
+                              if (datos == 'none'){
+                                this.cantidadpedidos = 0;
+                              }else{
+                                let cont = 0;
+                                for (let item of datos){
+                                  cont = cont + 1;
+                                }
+                                this.cantidadpedidos = cont;
+                              }
+                        })
+                  }else{
+                    this.cantidadpedidos = 0;
+                  }
+
+
+
+              if (this.keys.includes('visitas')){
+                this.StrCrl.get('visitas').then(datos =>{
+                      let cntcli = 0;
+                      let cntvisita = 0;
+                      for(let item of datos){
+                        cntvisita = cntvisita + 1;
+                            if(item['estado'] != 'Inicial'){
+                              cntcli = cntcli +1;
+                            }
+                      }
+
+                  this.cantidadClientes = cntvisita;
+                    this.cantidadClientesVisitados = cntcli;
+
+                })
+              }
+
+            })
+        })
+
+
+
   }
+ 
 
   IrMenu(){
     this.navCtrl.push(HomePage);
   }
 
+
+  /* recolectar el id del vendedor */
   DatosdeUsuario(){
     this.StrCrl.ready().then(()=>{
           this.StrCrl.get("udata").then(data =>{
@@ -42,6 +95,8 @@ export class SincronizarPage {
           });
     });
   }
+
+
 
   fecha() {
     let fecha = new Date();
@@ -67,47 +122,91 @@ export class SincronizarPage {
   }
 
 
+  /* sincronizar datos  */
 
   SincronizarVisitas(){
-    let direccion = this._conexion.Url + "visitas/" + this.vendedorId + "/sincronizar";
-    
-      this.StrCrl.ready().then(() => {
-
-          this.StrCrl.get("visitas").then(data => {
-                let aEnviar = {
-                  vendedorId: this.vendedorId,
-                  visitas: data
-                }
-                console.log(aEnviar)
-                this.http.post(direccion, aEnviar)
-                  .subscribe(datosRespuesta => {
-                    console.log(datosRespuesta);
-                   });
-
-          });
-
-      let direccionPedidos = this._conexion.Url + "pedidos/" + this.vendedorId +"/sincronizar" ;
-
-   
-
-      this.StrCrl.get("pedidos").then(data => {
-            let aEnviar2 = {
-              vendedorId: this.vendedorId,
-              fecha: new Date().toISOString().slice(0, 10),
-              pedidos: data
-            }
-
-        this.http.post(direccionPedidos, aEnviar2)
-          .subscribe(datosRespuesta => {
-            console.log(datosRespuesta)
-          }); 
-
-            
-      });
-
-
+    let loader = this.loadingCtrl.create({
+      content: "Descargando datos...",
     });
- 
+    loader.present();
+
+    this.TipoConexion = this.networkCtrl.type;
+/*verificar que este conectado a internet */
+    if (this.TipoConexion == 'none') {
+      loader.dismiss();
+      let alert = this.alertCtrl.create({
+        title: "Aviso !",
+        subTitle: "<p>No dispone de una <b>conexion a internet</b>. Conecte su dispositivo a internet e intente nuevamente</p>",
+        buttons: [
+          {
+            text: 'OK',
+            role: 'OK',
+          }
+        ]
+      });
+      alert.present();
+    }
+    else
+    {
+              let direccion = this._conexion.Url + "visitas/" + this.vendedorId + "/sincronizar";
+              this.StrCrl.ready().then(() => {
+
+                      this.StrCrl.get("visitas").then(data => {
+                          let aEnviar = {
+                            vendedorId: this.vendedorId,
+                            visitas: data
+                          }
+                          this.http.post(direccion, aEnviar)
+                            .subscribe(datosRespuesta => {
+                              console.log(datosRespuesta);
+                            });
+
+                        });
+
+
+                        let direccionPedidos = this._conexion.Url + "pedidos/" + this.vendedorId + "/sincronizar";
+                        this.StrCrl.get("pedidos").then(data => {
+                            if(data == 'none'){
+
+                            }else{
+                              let aEnviar2 = {
+                                vendedorId: this.vendedorId,
+                                fecha: new Date().toISOString().slice(0, 10),
+                                pedidos: data
+                              }
+                              this.http.post(direccionPedidos, aEnviar2)
+                                .subscribe(datosRespuesta => {
+                                  console.log(datosRespuesta);
+                                });
+                            }
+                        });
+              });
+
+              this.StrCrl.remove('pedidos');
+              this.StrCrl.remove('visitas');
+              this.cantidadpedidos   = '0';
+              this.cantidadClientes = '0';
+              this.cantidadClientesVisitados= '0';
+
+      loader.dismiss();
+
+      let alert = this.alertCtrl.create({
+        title: "Exelente !",
+        subTitle: "<p>Datos sincronizados <b>Correctamente</b>",
+        buttons: [
+          {
+            text: 'Continuar',
+            role: 'OK',
+          }
+        ]
+      });
+      alert.present();
+
+     }
+
   }
+
+
+
 
 }
